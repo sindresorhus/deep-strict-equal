b31e0f16a3076ac7f8dc6e303718438cf4992a87
Sam Verschueren <sam.verschueren@gmail.com>
2015-09-28 10:15:50 +0200

    Testing primitive types

--- test.js
+++ test.js
@@ -4,5 +4,7 @@ import fn from './';
 test(t => {
 	t.true(fn({foo: {bar: [1, 2]}}, {foo: {bar: [1, 2]}}));
 	t.false(fn({foo: {bar: [1, 2]}}, {foo: {bar: [1, 3]}}));
+	t.true(fn({foo: {bar: 1}}, {foo: {bar: 1}}));
+	t.false(fn({foo: {bar: 1}}, {foo: {bar: '1'}}));
 	t.end();
 });
